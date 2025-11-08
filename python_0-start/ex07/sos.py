9b4665cfc73fe865e32a67311a1c3d7229f2b4ca
osonseth <max.cesaria@gmail.com>
2025-11-08 22:09:34 +0100

    add: ex07

---
+++ python_0-start/ex07/sos.py
@@ -0,0 +1,65 @@
+import sys
+
+def input_into_morse_str(s: str) -> str:
+
+    MORSE_DICT = {
+        'A': '.-',
+        'B': '-...',
+        'C': '-.-.',
+        'D': '-..',
+        'E': '.',
+        'F': '..-.',
+        'G': '--.',
+        'H': '....',
+        'I': '..',
+        'J': '.---',
+        'K': '-.-',
+        'L': '.-..',
+        'M': '--',
+        'N': '-.',
+        'O': '---',
+        'P': '.--.',
+        'Q': '--.-',
+        'R': '.-.',
+        'S': '...',
+        'T': '-',
+        'U': '..-',
+        'V': '...-',
+        'W': '.--',
+        'X': '-..-',
+        'Y': '-.--',
+        'Z': '--..',
+
+        '0': '-----',
+        '1': '.----',
+        '2': '..---',
+        '3': '...--',
+        '4': '....-',
+        '5': '.....',
+        '6': '-....',
+        '7': '--...',
+        '8': '---..',
+        '9': '----.',
+
+        ' ': '/'
+    }
+    morse_str = " ".join(MORSE_DICT[c] for c in s)
+    return morse_str
+    
+def main():
+
+    try:
+        assert len(sys.argv) == 2, "the arguments are bad"
+        assert all(
+            char.isalnum() or char.isspace() for char in sys.argv[1]
+        ), "the arguments are bad"
+        input = sys.argv[1].upper()
+        morse_output = input_into_morse_str(input)
+        print(morse_output)
+       
+    except AssertionError as Error:
+        print(f"AssertionError: {Error}")
+
+
+if __name__ == "__main__":
+    main()
