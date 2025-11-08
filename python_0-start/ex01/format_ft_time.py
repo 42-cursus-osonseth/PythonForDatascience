7690496ed5803009d0e3e9ee4949dab49166bc32
osonseth <max.cesaria@gmail.com>
2025-11-08 09:28:51 +0100

    add: ex01

---
+++ python_0-start/ex01/format_ft_time.py
@@ -0,0 +1,7 @@
+import datetime as dt
+
+now = dt.datetime.today()
+ts = now.timestamp()
+print(f"Seconds since January 1, 1970: {ts:,.4f} or {ts:.2e} in scientific notation")
+now = dt.date.today()
+print(now)
